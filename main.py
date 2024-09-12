f5ae6d52c7ddb6233eb516f37bdf78568c6bb352
Aritra Sadhukhan <aritra.sadhukhan@codeclouds.co.in>
2024-09-12 20:24:09 +0530

    Dev - couple of logical fixes

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ def generate_pdfs():
   for i in range(len(data)):
     t = data[i]
     topic = t['topic']
-    gap = t['gap']
+    gap = t.get('gap', 5)
     del t['topic']
-    del t['gap']
+    t.pop("gap", None)
     print("Generating report for topic %s..." % topic)
-  filepath = f"./cache/{t['Subject Code']}.md"
-  if os.path.exists(filepath):
-    print("Reading cached markdown content...")
-    # Read the content of the file and set it to res
-    with open(filepath, 'r', encoding='utf-8') as file:
-      res = file.read()
-  else:
-    print("Generating markdown content from AI...")
-    res = ai.prompt(f'Please generate a long report for topic {topic} in context of {t['Subject']}' )
-    with open(filepath, 'w', encoding='utf-8') as file:
-      file.write(res)
-  print(f'Generating cover for labels: %s' % ', '.join([f'{key}: {value}' for key, value in t.items()]))
-  doc = cover.generate_doc_with_intro(topic,t, gap)
-  doc.add_page_break()
-  print("Converting ai provided data to docs format...")
-  html = md_to_html.convert(res)
-  html_to_doc.convert_and_add(html,doc)
-  doc.save(f'./docs/{t['Name']}_{t['Subject']}.docx')
-  print("Doc File generated successfully for task %s" % (i+1))
+    filepath = f"./cache/{t['Subject Code']}.md"
+    if os.path.exists(filepath):
+      print("Reading cached markdown content...")
+      # Read the content of the file and set it to res
+      with open(filepath, 'r', encoding='utf-8') as file:
+        res = file.read()
+    else:
+      print("Generating markdown content from AI...")
+      res = ai.prompt(f'Please generate a long report for topic {topic} in context of {t['Subject']}' )
+      with open(filepath, 'w', encoding='utf-8') as file:
+        file.write(res)
+    print(f'Generating cover for labels: %s \n' % ',\n '.join([f'{key}: {value}' for key, value in t.items()]))
+    doc = cover.generate_doc_with_intro(topic,t, gap)
+    doc.add_page_break()
+    print("Converting ai provided data to docs format...")
+    html = md_to_html.convert(res)
+    html_to_doc.convert_and_add(html,doc)
+    doc.save(f'./docs/{t['Subject']}_{t['Name']}.docx')
+    print("Doc File generated successfully for task %s" % (i+1))
 
 if(__name__ == '__main__'):
   generate_pdfs()
